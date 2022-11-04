f38029b3fa26ebca531af1b4774f2041d2b8592e
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2022-11-04 14:16:29 -0500

    this is the improved file

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,7 +1,10 @@
 use employees
    SELECT * FROM employees.employees
-   WHERE first_name = 'Irena'
-   OR first_name = 'Vidya'
-   OR first_name = 'Maya'
-   AND gender ='M'
-   LIMIT 25;
+       WHERE first_name LIKE 'E%'
+       OR first_name LIKE '%E'
+       OR first_name = 'Irena'
+       OR first_name = 'Vidya'
+       OR first_name = 'Maya'
+       AND gender ='M'
+       LIMIT 25;
+
