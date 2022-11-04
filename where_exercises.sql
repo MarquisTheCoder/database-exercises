c86f8a52a1efcf26fcc3b46332c079429a64ee0a
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2022-11-04 14:22:01 -0500

    adding new clauses

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,10 +1,12 @@
 use employees
    SELECT * FROM employees.employees
        WHERE first_name LIKE 'E%'
+       OR last_name LIKE '%q%'
        OR first_name LIKE '%E'
        OR first_name = 'Irena'
        OR first_name = 'Vidya'
        OR first_name = 'Maya'
        AND gender ='M'
-       LIMIT 25;
+       AND last_name NOT LIKE '%qu%'
+   LIMIT 25;
 
