c450edc2759dfc9498b067317f7dde997df466c5
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2022-11-08 09:23:33 -0600

    testing git config

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,4 +1,5 @@
 user employees;
 
+--jdjjdsfkljbfsbdf
 select concat(first_name, ' ', last_name) from employees.employees
     where hire_date in (select hire_date from employees where emp_no = 101010);
